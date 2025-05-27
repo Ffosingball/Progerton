40d910c4fe3913a7f02927e049f5eac31bb3ff69
Ffosingball <andrii2830@gmail.com>
2025-05-27 11:53:35 +0100

    Added tutorial to my game

--- TutorialManager.cs
+++ TutorialManager.cs
@@ -1,16 +1,76 @@
 using UnityEngine;
+using UnityEngine.Localization;
+using UnityEngine.Localization.Tables;
+using UnityEngine.UI;
+using TMPro;
 
 public class TutorialManager : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    [SerializeField]
+    private int numOfPages = 3;
+    [SerializeField]
+    private TMP_Text pageText, mainText;
+    [SerializeField]
+    private LocalizedStringTable _localizedStringTable;
+    private StringTable _currentStringTable;
+
+
+    public SettingsManager settingsManager;
+
+
+    private int currentPage;
+
+
     void Start()
     {
-        
+        if(GameInfo.otherGameInfo==null)
+            GameInfo.getOtherInfo();
+            
+        GameInfo.otherGameInfo.startedTraining = true;
+        currentPage = 0;
+        updateText();
     }
 
-    // Update is called once per frame
+
     void Update()
     {
-        
+        // Left mouse button click (0)
+        if (Input.GetMouseButtonDown(0) && Time.timeScale == 1f)
+        {
+            if (currentPage + 1 < numOfPages)
+            {
+                currentPage++;
+                updateText();
+            }
+
+            if (currentPage + 1 == numOfPages)
+            { 
+                GameInfo.otherGameInfo.finishedTraining = true;
+            }
+        }
+
+        // Right mouse button click (1)
+        if (Input.GetMouseButtonDown(1) && Time.timeScale==1f)
+        {
+            if (currentPage - 1 >= 0)
+            {
+                currentPage--;
+                updateText();
+            }
+        }
+
+        if (settingsManager.getLanguageChanged())
+        {
+            updateText();
+        }
+    }
+
+
+    private void updateText()
+    {
+        _currentStringTable = _localizedStringTable.GetTable();
+
+        pageText.text = (currentPage + 1) +" "+ _currentStringTable["of"].LocalizedValue +" "+ numOfPages;
+        mainText.text = _currentStringTable["p"+currentPage].LocalizedValue;
     }
 }
